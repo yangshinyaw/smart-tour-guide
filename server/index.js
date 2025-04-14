f54ba42decffb8f70041835ce6a887dcf7478aa0
Shin <syty03@gmail.com>
2025-04-15 00:57:25 +0800

    backend set up

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// server/index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('SmartTour API is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
